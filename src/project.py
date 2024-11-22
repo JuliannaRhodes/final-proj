fc3ae14089a85ab5830798860ac70bb3bb7d148a
Julianna Rhodes <juliannarhodes33@gmail.com>
2024-11-22 17:29:51 -0600

    Added paddle class and details

--- src/project.py
+++ src/project.py
@@ -24,6 +24,17 @@ class Ball:
     def draw(self, screen):
         pygame.draw.circle(screen, self.color, (int(self.x), int(self.y)), self.radius)
 
+
+class Paddle:
+    def __init__(self, x, y, width, height, color):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.color = color
+        self.speed = 5
+
+
 def function_1():
     ...
 
